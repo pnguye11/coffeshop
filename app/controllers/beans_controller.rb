792d8ab52a863215011fa1849811a31374ac53dc
Phuong Nathan Nguyen <pnguye11@gmail.com>
2016-05-27 15:13:44 -0700

    resources

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -9,11 +9,33 @@ class BeansController < ApplicationController
     @bean = Bean.new
   end
   def create
-    @bean = Bean.new(params.require(:bean).permit(:name, :roast, :origin, :quanity))
+    @bean = Bean.new(bean_params)
     if @bean.save
         redirect_to beans_path
     else
       render :new
     end
+  end
+  def edit
+    @bean = Bean.find(params[:id])
+  end
+  def update
+    @bean = Bean.find(params[:id])
+    if @bean.update_attributes(bean_params)
+    redirect_to beans_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @bean = Bean.find(params[:id])
+    @bean.destroy
+    redirect_to beans_path
+  end
+
+  private
+  def bean_params
+    params.require(:bean).permit(:name, :roast, :origin, :quanity)
+
   end
 end
